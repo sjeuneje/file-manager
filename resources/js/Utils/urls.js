77f3945190f3b9df6e14a6206755c2a49f32d608
Sacha Jeunejean <sachajeunejean@MacBook-Pro-de-Sacha-2.local>
2024-07-22 13:46:12 +0200

    refactor getParentId util

--- resources/js/Utils/urls.js
+++ resources/js/Utils/urls.js
@@ -1,3 +1,11 @@
 export default function getParentId() {
+    const queryString = window.location.search;
+
+    if (
+        !queryString.substring(1).includes('parent_id') ||
+        window.location.search.substring(1)?.split('=').length > 2
+    )
+        return null;
+
     return window.location.search.substring(1)?.split('=')?.pop();
 }
