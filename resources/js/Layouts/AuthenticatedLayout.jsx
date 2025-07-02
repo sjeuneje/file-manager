cc6871834e5b2009d7b27aecda3d3a9e9cf87dbe
Sacha Jeunejean <sacha.jeunejean@gmail.com>
2025-07-02 23:17:36 +0200

    comment future features

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -18,22 +18,22 @@ const mainElements = [
         text: "Mon Drive",
         href: "/dashboard"
     },
-    {
-        id: 2,
-        icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
-        </svg>,
-        text: "Partagé avec moi",
-        href: "/shared-with-me"
-    },
-    {
-        id: 3,
-        icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-        </svg>,
-        text: "Récent",
-        href: "/latest"
-    },
+    // {
+    //     id: 2,
+    //     icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    //         <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+    //     </svg>,
+    //     text: "Partagé avec moi",
+    //     href: "/shared-with-me"
+    // },
+    // {
+    //     id: 3,
+    //     icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    //         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    //     </svg>,
+    //     text: "Récent",
+    //     href: "/latest"
+    // },
     {
         id: 4,
         icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -173,66 +173,66 @@ export default function Authenticated({ user, header, children }) {
                                   )
                               })}
                           </ul>
-                          <div className="mt-4">
-                              <h3 className="text-gray-400 text-sm tracking-wider uppercase">Stockage</h3>
-                          </div>
-                          <ul className="flex flex-col mt-4 gap-3">
-                              {storageElements.map((el) => {
-                                  if (el.href === '/storage') {
-                                      return (
-                                          <Link
-                                              key={el.id}
-                                              // href={el.href}
-                                              className={router?.page?.url === el.href ? "flex flex-row items-center gap-2 bg-indigo-100 text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg" : "flex flex-row items-center gap-2 text-gray-600 hover:bg-gray-200 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg"}
-                                          >
-                                              {el.icon}
-                                              <span>{el.text}</span>
-                                          </Link>
-                                      )
-                                  }
+                          {/*<div className="mt-4">*/}
+                          {/*    <h3 className="text-gray-400 text-sm tracking-wider uppercase">Stockage</h3>*/}
+                          {/*</div>*/}
+                          {/*<ul className="flex flex-col mt-4 gap-3">*/}
+                          {/*    {storageElements.map((el) => {*/}
+                          {/*        if (el.href === '/storage') {*/}
+                          {/*            return (*/}
+                          {/*                <Link*/}
+                          {/*                    key={el.id}*/}
+                          {/*                    // href={el.href}*/}
+                          {/*                    className={router?.page?.url === el.href ? "flex flex-row items-center gap-2 bg-indigo-100 text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg" : "flex flex-row items-center gap-2 text-gray-600 hover:bg-gray-200 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg"}*/}
+                          {/*                >*/}
+                          {/*                    {el.icon}*/}
+                          {/*                    <span>{el.text}</span>*/}
+                          {/*                </Link>*/}
+                          {/*            )*/}
+                          {/*        }*/}
 
-                                  return (
-                                      <Link
-                                          key={el.id}
-                                          href={el.href}
-                                          className={router?.page?.url === el.href ? "flex flex-row items-center gap-2 bg-indigo-100 text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg" : "flex flex-row items-center gap-2 text-gray-600 hover:bg-indigo-100 hover:text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg"}
-                                      >
-                                          {el.icon}
-                                          <span>{el.text}</span>
-                                      </Link>
-                                  )
-                              })}
-                          </ul>
-                          <div className="mt-4">
-                              <h3 className="text-gray-400 text-sm tracking-wider uppercase">Paramètres</h3>
-                          </div>
-                          <ul className="flex flex-col mt-4 gap-3">
-                              {settingsElements.map((el) => {
-                                  if (el.href === '/settings') {
-                                      return (
-                                          <Link
-                                              key={el.id}
-                                              // href={el.href}
-                                              className={router?.page?.url === el.href ? "flex flex-row items-center gap-2 bg-indigo-100 text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg" : "flex flex-row items-center gap-2 text-gray-600 hover:bg-gray-200 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg"}
-                                          >
-                                              {el.icon}
-                                              <span>{el.text}</span>
-                                          </Link>
-                                      )
-                                  }
+                          {/*        return (*/}
+                          {/*            <Link*/}
+                          {/*                key={el.id}*/}
+                          {/*                href={el.href}*/}
+                          {/*                className={router?.page?.url === el.href ? "flex flex-row items-center gap-2 bg-indigo-100 text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg" : "flex flex-row items-center gap-2 text-gray-600 hover:bg-indigo-100 hover:text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg"}*/}
+                          {/*            >*/}
+                          {/*                {el.icon}*/}
+                          {/*                <span>{el.text}</span>*/}
+                          {/*            </Link>*/}
+                          {/*        )*/}
+                          {/*    })}*/}
+                          {/*</ul>*/}
+                          {/*<div className="mt-4">*/}
+                          {/*    <h3 className="text-gray-400 text-sm tracking-wider uppercase">Paramètres</h3>*/}
+                          {/*</div>*/}
+                          {/*<ul className="flex flex-col mt-4 gap-3">*/}
+                          {/*    {settingsElements.map((el) => {*/}
+                          {/*        if (el.href === '/settings') {*/}
+                          {/*            return (*/}
+                          {/*                <Link*/}
+                          {/*                    key={el.id}*/}
+                          {/*                    // href={el.href}*/}
+                          {/*                    className={router?.page?.url === el.href ? "flex flex-row items-center gap-2 bg-indigo-100 text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg" : "flex flex-row items-center gap-2 text-gray-600 hover:bg-gray-200 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg"}*/}
+                          {/*                >*/}
+                          {/*                    {el.icon}*/}
+                          {/*                    <span>{el.text}</span>*/}
+                          {/*                </Link>*/}
+                          {/*            )*/}
+                          {/*        }*/}
 
-                                  return (
-                                      <Link
-                                          key={el.id}
-                                          href={el.href}
-                                          className={router?.page?.url === el.href ? "flex flex-row items-center gap-2 bg-indigo-100 text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg" : "flex flex-row items-center gap-2 text-gray-600 hover:bg-indigo-100 hover:text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg"}
-                                      >
-                                          {el.icon}
-                                          <span>{el.text}</span>
-                                      </Link>
-                                  )
-                              })}
-                          </ul>
+                          {/*        return (*/}
+                          {/*            <Link*/}
+                          {/*                key={el.id}*/}
+                          {/*                href={el.href}*/}
+                          {/*                className={router?.page?.url === el.href ? "flex flex-row items-center gap-2 bg-indigo-100 text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg" : "flex flex-row items-center gap-2 text-gray-600 hover:bg-indigo-100 hover:text-indigo-500 ease-in-out duration-150 cursor-pointer px-4 py-3 rounded-lg"}*/}
+                          {/*            >*/}
+                          {/*                {el.icon}*/}
+                          {/*                <span>{el.text}</span>*/}
+                          {/*            </Link>*/}
+                          {/*        )*/}
+                          {/*    })}*/}
+                          {/*</ul>*/}
                       </div>
                   </div>
               </div>
